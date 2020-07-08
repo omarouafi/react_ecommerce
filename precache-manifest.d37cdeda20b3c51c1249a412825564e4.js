57da323dbaf4a54a696820f6c214453a57078f23
omarouafi <omarouafi12@gmail.com>
2020-07-08 16:04:49 +0000

    Updates

---
+++ precache-manifest.d37cdeda20b3c51c1249a412825564e4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ae725f667abfbb824e74df2cf270e",
+    "url": "/react_ecommerce/index.html"
+  },
+  {
+    "revision": "1f2159c81b5e67943f65",
+    "url": "/react_ecommerce/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1f4f8f2036a310898915",
+    "url": "/react_ecommerce/static/js/2.6018adcf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_ecommerce/static/js/2.6018adcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f2159c81b5e67943f65",
+    "url": "/react_ecommerce/static/js/main.c8ee6b6d.chunk.js"
+  },
+  {
+    "revision": "b0798869d4699862c8f2",
+    "url": "/react_ecommerce/static/js/runtime-main.df5c09df.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react_ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
